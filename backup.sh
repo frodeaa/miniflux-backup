e4477c6fd1af9dad3345f12ec2925aed1ea16a88
Frode Aannevik <frode.aa@gmail.com>
2018-03-17 22:58:01 +0100

    Sat Mar 17 22:58:01 CET 2018 miniflux backup
    Sat Mar 17 23:01:52 CET 2018 miniflux backup
    
    Sat Mar 17 23:02:26 CET 2018 miniflux backup
    
    Sun 18 Mar 01:00:03 CET 2018 miniflux backup
    
    Mon 19 Mar 01:00:03 CET 2018 miniflux backup
    
    Tue 20 Mar 01:00:02 CET 2018 miniflux backup
    
    Wed 21 Mar 01:00:03 CET 2018 miniflux backup
    
    Thu 22 Mar 01:00:03 CET 2018 miniflux backup
    
    Fri 23 Mar 01:00:03 CET 2018 miniflux backup
    
    Sat 24 Mar 01:00:04 CET 2018 miniflux backup
    
    Sun 25 Mar 01:00:04 CET 2018 miniflux backup
    
    Mon 26 Mar 01:00:05 CEST 2018 miniflux backup
    
    Tue 27 Mar 01:00:04 CEST 2018 miniflux backup
    
    Wed 28 Mar 01:00:04 CEST 2018 miniflux backup
    
    Thu 29 Mar 01:00:05 CEST 2018 miniflux backup
    
    Fri 30 Mar 01:00:04 CEST 2018 miniflux backup
    
    Sat 31 Mar 01:00:05 CEST 2018 miniflux backup

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
+cd $(dirname "$0")
 pg_dump -U miniflux_user -h localhost -d miniflux > miniflux_backup.sql
 git add .
-git commit -m "${TODAY}" --quiet 
-git push origin master
+git commit -m "$(date) miniflux backup" --quiet 
+git push origin master --quiet
