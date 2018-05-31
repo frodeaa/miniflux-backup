ead494dd25899bbbe88bc8ea02c2c89dba25b342
Frode Aannevik <frode.aa@gmail.com>
2018-06-01 01:00:28 +0200

    Fri  1 Jun 01:00:28 CEST 2018 miniflux backup
    Sat  2 Jun 01:00:18 CEST 2018 miniflux backup
    
    Sun Jun  3 01:00:24 CEST 2018 miniflux backup
    
    Mon Jun  4 01:00:23 CEST 2018 miniflux backup
    
    Tue Jun  5 01:00:25 CEST 2018 miniflux backup
    
    Wed Jun  6 01:00:27 CEST 2018 miniflux backup
    
    Thu Jun  7 01:00:27 CEST 2018 miniflux backup
    
    Fri Jun  8 01:00:31 CEST 2018 miniflux backup
    
    Sat Jun  9 01:00:35 CEST 2018 miniflux backup
    
    Sun Jun 10 01:00:27 CEST 2018 miniflux backup
    
    Mon Jun 11 01:00:33 CEST 2018 miniflux backup
    
    Tue Jun 12 01:00:34 CEST 2018 miniflux backup
    
    Wed Jun 13 01:00:34 CEST 2018 miniflux backup
    
    Thu Jun 14 01:00:30 CEST 2018 miniflux backup
    
    Fri Jun 15 01:00:30 CEST 2018 miniflux backup
    
    Sat Jun 16 01:00:32 CEST 2018 miniflux backup
    
    Sun Jun 17 01:00:30 CEST 2018 miniflux backup
    
    Mon Jun 18 01:00:29 CEST 2018 miniflux backup
    
    Tue Jun 19 01:00:29 CEST 2018 miniflux backup
    
    Wed Jun 20 01:00:29 CEST 2018 miniflux backup
    
    Thu Jun 21 01:00:27 CEST 2018 miniflux backup
    
    Thu Jun 21 23:44:42 CEST 2018 miniflux backup
    
    compress the backup to avoid GitHub 100MB limit
    
    Fri Jun 22 19:13:44 CEST 2018 miniflux backup
    
    Sat Jun 23 01:00:29 CEST 2018 miniflux backup
    
    Sun Jun 24 01:00:28 CEST 2018 miniflux backup
    
    Mon Jun 25 01:00:28 CEST 2018 miniflux backup
    
    Tue Jun 26 01:00:28 CEST 2018 miniflux backup
    
    Wed Jun 27 01:00:28 CEST 2018 miniflux backup
    
    Thu Jun 28 01:00:28 CEST 2018 miniflux backup
    
    Fri Jun 29 01:00:28 CEST 2018 miniflux backup
    
    Sat Jun 30 01:00:29 CEST 2018 miniflux backup

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 cd $(dirname "$0")
-pg_dump -U miniflux_user -h localhost -d miniflux > miniflux_backup.sql
+pg_dump -U miniflux_user -h localhost -d miniflux | gzip -9 > miniflux_backup.sql.gz
 git add .
 git commit -m "$(date) miniflux backup" --quiet 
 git push origin master --quiet
