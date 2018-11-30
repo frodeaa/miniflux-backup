77a96ef659cf8dcd83d10909c16cb9124d8b4799
Frode Aannevik <frode.aa@gmail.com>
2018-11-30 23:03:54 +0100

    rewrite the history and encrypt the backup

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 cd $(dirname "$0")
-pg_dump -U miniflux_user -h localhost -d miniflux | gzip -9 > miniflux_backup.sql.gz
+pg_dump -U miniflux_user -h localhost -d miniflux \
+    | gzip -9 | gpg -e -r 'CFFA2237F1526F8F8024EAC918F1D5F4837B359D' > miniflux_backup.sql.gz.enc
 git add .
-git commit -m "$(date) miniflux backup" --quiet 
+git commit -m "$(date) miniflux backup" --quiet
 git push origin master --quiet
