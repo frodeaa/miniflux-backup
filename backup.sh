d5cd2dbcc843b4cf03cbca1a31f2bf7708fe9c47
Frode Aannevik <frode.aa@gmail.com>
2018-12-01 12:53:43 +0100

    set -eo pipefail to abort on error

--- backup.sh
+++ backup.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
 cd $(dirname "$0")
 pg_dump -U miniflux_user -h localhost -d miniflux \
     | gzip -9 | gpg -e -r 'CFFA2237F1526F8F8024EAC918F1D5F4837B359D' > miniflux_backup.sql.gz.enc
